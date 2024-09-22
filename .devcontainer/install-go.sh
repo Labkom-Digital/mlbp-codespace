c61cada6f3b4e78703215087f0f6ff9c95133f37
Alfian Isnan <49936990+alfianisnan26@users.noreply.github.com>
2024-09-22 07:53:12 +0000

    chore: fix dockerfile and add installer bash

--- .devcontainer/install-go.sh
+++ .devcontainer/install-go.sh
@@ -343,16 +343,4 @@ find "${TARGET_GOPATH}" -type d -print0 | xargs -n 1 -0 chmod g+s
 # Clean up
 clean_up
 
-# Install Gophernotes
-export CURRENT_PATH=$(pwd)
-
-go install github.com/gopherdata/gophernotes@v0.7.5
-mkdir -p ~/.local/share/jupyter/kernels/gophernotes
-cd ~/.local/share/jupyter/kernels/gophernotes
-cp "$(go env GOPATH)"/pkg/mod/github.com/gopherdata/gophernotes@v0.7.5/kernel/*  "."
-chmod +w ./kernel.json # in case copied kernel.json has no write permission
-sed "s|gophernotes|$(go env GOPATH)/bin/gophernotes|" < kernel.json.in > kernel.json
-
-cd $CURRENT_PATH
-
 echo "Done!"
