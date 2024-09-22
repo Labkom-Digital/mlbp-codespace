ad684b6f335d0a335c74ec5abb659f41ec49f1e2
Alfian Isnan <49936990+alfianisnan26@users.noreply.github.com>
2024-09-22 12:11:24 +0000

    chore: remove unnecessary adjust gophernotes

--- .devcontainer/install-gophernotes.sh
+++ .devcontainer/install-gophernotes.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-
-
 go install github.com/gopherdata/gophernotes@v0.7.5
-mkdir -p ~/.local/share/jupyter/kernels/gophernotes
-cd ~/.local/share/jupyter/kernels/gophernotes
+mkdir -p /home/vscode/.local/share/jupyter/kernels/gophernotes
+cd /home/vscode/.local/share/jupyter/kernels/gophernotes
 cp "$(go env GOPATH)"/pkg/mod/github.com/gopherdata/gophernotes@v0.7.5/kernel/*  "."
 chmod +w ./kernel.json # in case copied kernel.json has no write permission
 sed "s|gophernotes|$(go env GOPATH)/bin/gophernotes|" < kernel.json.in > kernel.json
